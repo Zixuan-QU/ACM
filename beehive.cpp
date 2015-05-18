580d1095075214a24f682016ee92ab2cefb1b1df
Zixuan QU <qzx1107@gmail.com>
2015-05-18 17:04:43 +0200

    Beehive

--- beehive.cpp
+++ beehive.cpp
@@ -11,24 +11,19 @@
 #include<iomanip>
 using namespace std;
 
-void bfs(vector<vector<int> > &graph,vector<int> &visit,vector<int> father,int node,int &minl){
+void dfs(vector<vector<int> > &graph,vector<int> &depth,int father,int level,int node,int &minl){
+	depth[node] = level;
+	for(auto v : graph[node]){
+		if(v==father) continue;
+		if(depth[v]!=-1){
+			if(depth[v]<level)
+				minl= min(minl,level-depth[v]+1);
+			else if(depth[v]>level+1)
+				dfs(graph,depth,node,level+1,v,minl);
+		}else
+			dfs(graph,depth,node,level+1,v,minl);
 
-	visit[node] = 1;
-	for(int i =0;i<graph[node].size();i++){
-		if(visit[graph[node][i]]==1 && i!=father[node]){
-			int tmp = node,l=1;
-			while(tmp!=i){
-				tmp = father[tmp];
-				l++;
-			}
-			if(minl>l) minl = l;
-		}
-		else if(visit[graph[node][i]]==0){
-			father[graph[node][i]]=node;
-			bfs(graph,visit,father,graph[node][i],minl);
-		}
 	}
-	visit[node] = 2;
 }
 
 int main(){
@@ -36,29 +31,29 @@ int main(){
 	freopen("input","r+",stdin);
 
 	int ncases;
-	cin>>ncases;
+	scanf("%d", &ncases);
 	int count=0;
 	while(++count<=ncases){
 		int t,p;
-		cin>>t>>p;
+		scanf("%d", &t);
+		scanf("%d", &p);
 		vector<vector<int> >graph(t);
 		while(p--){
 			int start,end;
-			cin>>start>>end;
+			scanf("%d", &start);
+			scanf("%d", &end);
 			graph[start].push_back(end);
 			graph[end].push_back(start);
 		}
-		vector<int> visit(t,0);
-		vector<int> father(t,-1);
+		vector<int> depth(t,-1);
 		int minl = INT_MAX;
 		for(int i = 0;i<t;i++){
-			if(!visit[i])
-				bfs(graph,visit,father,i,minl);
+			if(depth[i]==-1)
+				dfs(graph,depth,-1,1,i,minl);
 		}
-		cout<<"Case "<<count<<": ";
-		if(minl==INT_MAX) cout<<"impossible"<<endl;
-		else cout<<minl<<endl;
-		if(count!=ncases) cout<<endl;
+		printf("Case %d: ",count);
+		if(minl==INT_MAX) printf("impossible\n");
+		else printf("%d\n",minl);
 	}
 	return 0;
 }
